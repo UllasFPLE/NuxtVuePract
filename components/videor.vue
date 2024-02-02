bbb5a867f4001cd172665fcd7ef77408dcae3367
UllasFPLE <ullasjs@fple.in>
2024-02-02 12:12:20 +0530

    updated

--- components/videor.vue
+++ components/videor.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="rounded-[20px] overflow-clip relative" @mouseenter="makeTrue" @mouseleave="makeFalse">
-    <img class="pic_blur rounded-[20px] hover:blur-md transition-all duration-300"
-      :class="{ 'blur-md': Hovering, '': !Hovering }" :src="image" alt="">
+  <div class="rounded-[20px] overflow-clip relative" @mouseenter="makeTrue(1)" @mouseleave="makeFalse">
+    <video class="pic_blur rounded-[20px] transition-all duration-300 absolute scale-110" :class="{ 'blur-md': Hovering, '': !Hovering }"
+      :src="video" alt="" autoplay loop @mouseover="hoverBlur" @mouseout="removeBlur"></video>
     <div
       class="text_in flex absolute transition-all duration-300 w-full justify-between items-center top-[6%] px-8 left-0 z-10 text-white"
       :class="{ 'opacity-0': !hoverEd, 'opacity-100': hoverEd }" @mouseenter="hoverBlur" @mouseleave="removeBlur">
       <p class="text-[20px] text-white">{{ word }}</p>
-      <nuxt-link :to="'/about/?image=' + image + '&name=' + word"
+      <nuxt-link :to="'/about/?image=' + video + '&name=' + word"
         class="material-icons text-[18px] bg-white text-black p-3 rounded-full hover:shadow-xl transition-all duration-[400ms]"
         :class="{ 'translate-y-[15px] tran,,,,,slate-x-[-15px]': !hoverEd, 'translate]': hoverEd }">north_east</nuxt-link>
     </div>
@@ -25,7 +25,7 @@ const removeBlur = () => {
   Hovering.value = false;
 }
 
-const { image, word } = defineProps(['image', 'word'])
+const { video, word } = defineProps(['video', 'word'])
 
 const makeTrue = () => {
   hoverEd.value = true;
